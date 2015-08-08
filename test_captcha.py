96f925ef3ed67ff7ce842e36246e283626d0c6f0
yapadubpadoo <yapadubpadoo@gmail.com>
2015-08-08 13:49:52 +0700

    init test cases TestFirstPatternRightOperand

--- test_captcha.py
+++ test_captcha.py
@@ -43,6 +43,32 @@ class TestSecondPatternLeftOperand(unittest.TestCase):
 		captcha = Captcha(first_pattern, focus, dummy_operator, dummy_right)
 		self.assertEqual('NINE', captcha.getLeft())
 
+class TestFirstPatternRightOperand(unittest.TestCase):
+	def test_1_should_be_1(self):
+		first_pattern = 2
+		dummy_left = 9
+		dummy_operator = 1
+		focus = 1
+		captcha = Captcha(first_pattern, dummy_left, dummy_operator, focus)
+		self.assertEqual('1', captcha.getRight())
+
+	def test_2_should_be_2(self):
+		first_pattern = 2
+		dummy_left = 9
+		dummy_operator = 1
+		focus = 2
+		captcha = Captcha(first_pattern, dummy_left, dummy_operator, focus)
+		self.assertEqual('2', captcha.getRight())
+
+	def test_9_should_be_9(self):
+		first_pattern = 2
+		dummy_left = 9
+		dummy_operator = 1
+		focus = 9
+		captcha = Captcha(first_pattern, dummy_left, dummy_operator, focus)
+		self.assertEqual('9', captcha.getRight())
+
+# class TestSecondPatternRightOperand(unittest.TestCase):
 
 
 

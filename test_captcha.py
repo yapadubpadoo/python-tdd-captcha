2392725d33843d0c6890398d779798a3a5f7789d
yapadubpadoo <yapadubpadoo@gmail.com>
2015-08-08 13:53:23 +0700

    rename parameter

--- test_captcha.py
+++ test_captcha.py
@@ -20,52 +20,52 @@ class TestFirstPatternLeftOperand(unittest.TestCase):
 class TestSecondPatternLeftOperand(unittest.TestCase):
 
 	def test_2_should_be_TWO(self):
-		first_pattern = 2
+		pattern = 2
 		focus = 2
 		dummy_operator = 1
 		dummy_right = 1
-		captcha = Captcha(first_pattern, focus, dummy_operator, dummy_right)
+		captcha = Captcha(pattern, focus, dummy_operator, dummy_right)
 		self.assertEqual('TWO', captcha.getLeft())
 
 	def test_5_should_be_FIVE(self):
-		first_pattern = 2
+		pattern = 2
 		focus = 5
 		dummy_operator = 1
 		dummy_right = 1
-		captcha = Captcha(first_pattern, focus, dummy_operator, dummy_right)
+		captcha = Captcha(pattern, focus, dummy_operator, dummy_right)
 		self.assertEqual('FIVE', captcha.getLeft())
 
 	def test_9_should_be_NINE(self):
-		first_pattern = 2
+		pattern = 2
 		focus = 9
 		dummy_operator = 1
 		dummy_right = 1
-		captcha = Captcha(first_pattern, focus, dummy_operator, dummy_right)
+		captcha = Captcha(pattern, focus, dummy_operator, dummy_right)
 		self.assertEqual('NINE', captcha.getLeft())
 
 class TestFirstPatternRightOperand(unittest.TestCase):
 	def test_1_should_be_1(self):
-		first_pattern = 1
+		pattern = 1
 		dummy_left = 9
 		dummy_operator = 1
 		focus = 1
-		captcha = Captcha(first_pattern, dummy_left, dummy_operator, focus)
+		captcha = Captcha(pattern, dummy_left, dummy_operator, focus)
 		self.assertEqual('1', captcha.getRight())
 
 	def test_2_should_be_2(self):
-		first_pattern = 1
+		pattern = 1
 		dummy_left = 9
 		dummy_operator = 1
 		focus = 2
-		captcha = Captcha(first_pattern, dummy_left, dummy_operator, focus)
+		captcha = Captcha(pattern, dummy_left, dummy_operator, focus)
 		self.assertEqual('2', captcha.getRight())
 
 	def test_9_should_be_9(self):
-		first_pattern = 1
+		pattern = 1
 		dummy_left = 9
 		dummy_operator = 1
 		focus = 9
-		captcha = Captcha(first_pattern, dummy_left, dummy_operator, focus)
+		captcha = Captcha(pattern, dummy_left, dummy_operator, focus)
 		self.assertEqual('9', captcha.getRight())
 
 # class TestSecondPatternRightOperand(unittest.TestCase):
